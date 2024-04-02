4ffd5e830a76032030e24b5b7d26de2111bff854
betocostadev <robertomarx@gmail.com>
2024-04-02 18:11:34 -0300

    Added template files for some sorting

---
+++ specs/bubble-sort/template-bubble-sort.test.js
@@ -0,0 +1,16 @@
+// Bubble sort template
+// Do not write your code here, copy this file.
+
+function bubbleSort(nums) {
+  // code goes here
+}
+
+// unit tests
+// do not modify the below code
+test.skip('TEMPLATE bubble sort', function () {
+  const nums = [10, 14, 3, 11, 8, 2, 12, 13, 5, 6, 4, 7, 15, 9, 1, 0]
+  const sortedNums = bubbleSort(nums)
+  expect(sortedNums).toEqual([
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+  ])
+})
---
+++ specs/insertion-sort/template-insertion-sort.test.js
@@ -0,0 +1,14 @@
+// Insertion sort template
+// Do not write your code here, copy this file.
+
+function insertionSort(nums) {
+  // code goes here
+}
+
+// unit tests
+// do not modify the below code
+test.skip('TEMPLATE insertion sort', function () {
+  const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1]
+  insertionSort(nums)
+  expect(nums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+})
