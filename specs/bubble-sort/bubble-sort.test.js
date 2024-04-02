22632cd5a8f6d424ba8ba08074c69e3e0d39a4b9
betocostadev <robertomarx@gmail.com>
2024-04-02 16:40:32 -0300

    Solved insertion sort

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -33,7 +33,9 @@ function bubbleSort(nums) {
 // unit tests
 // do not modify the below code
 test('bubble sort', function () {
-  const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1]
+  const nums = [10, 14, 3, 11, 8, 2, 12, 13, 5, 6, 4, 7, 15, 9, 1, 0]
   const sortedNums = bubbleSort(nums)
-  expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+  expect(sortedNums).toEqual([
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+  ])
 })
--- src/Sort.js
+++ src/Sort.js
@@ -4,18 +4,30 @@ import { App, snapshot, done, clear } from './sort-visualizer'
 
 import './sort.css'
 
-function sort(array) {
+function sort(nums) {
   // do cool stuff here
-
   // call snapshot any time you do anything to the array
   // it's okay if you call it with duplicate value array,
   // it will deduplicate for you
-  snapshot(array)
+  // Checking with insertion sort
+  snapshot(nums)
+  for (let i = 1; i < nums.length; i++) {
+    let numToInsert = nums[i]
+    let j
+
+    for (j = i - 1; nums[j] > numToInsert && j >= 0; j--) {
+      nums[j + 1] = nums[j]
+    }
+
+    nums[j + 1] = numToInsert
+    snapshot(nums)
+  }
+  return nums
 }
 
 export default function SortComponent() {
   clear()
-  sort(shuffle(range(10)))
+  sort(shuffle(range(16)))
   done()
   return <App />
 }
