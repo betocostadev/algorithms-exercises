c3f502f4c7b112400c95f3e47cadae6a7b6e6103
betocostadev <robertomarx@gmail.com>
2024-04-02 15:41:34 -0300

    Solved bubbleSort

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -12,11 +12,27 @@
 
 function bubbleSort(nums) {
   // code goes here
+  let swapped = false
+  let size = nums.length
+  do {
+    swapped = false
+    for (let i = 0; i < size; i++) {
+      if (nums[i] > nums[i + 1]) {
+        let selected = nums[i]
+        nums[i] = nums[i + 1]
+        nums[i + 1] = selected
+        swapped = true
+      }
+    }
+    // reduce the size of the array by 1 - we know the last element is in the correct place
+    size--
+  } while (swapped)
+  return nums
 }
 
 // unit tests
 // do not modify the below code
-test.skip('bubble sort', function () {
+test('bubble sort', function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1]
   const sortedNums = bubbleSort(nums)
   expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
