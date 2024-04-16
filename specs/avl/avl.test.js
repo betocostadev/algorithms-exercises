9f5f0ea3d7ca3c81b3dce050554218c537051cdc
betocostadev <robertomarx@gmail.com>
2024-04-16 15:27:06 -0300

    Completed AVL Trees balancing

--- specs/avl/avl.test.js
+++ specs/avl/avl.test.js
@@ -52,16 +52,26 @@ class Node {
 
   add(value) {
     if (value < this.value) {
+      // go left
       if (this.left) {
         this.left.add(value)
       } else {
         this.left = new Node(value)
       }
+      // if the left is taller than the right
+      if (!this.right || this.right.height < this.left.height) {
+        this.height = this.left.height + 1
+      }
     } else {
+      // go right
       if (this.right) {
         this.right.add(value)
       } else {
         this.right = new Node(value)
+      }
+      // if the right is taller than the left
+      if (!this.left || this.left.height < this.right.height) {
+        this.height = this.right.height + 1
       }
     }
     this.balance()
@@ -69,32 +79,80 @@ class Node {
 
   balance() {
     // ask if is this node out of balance
+    const rightHeight = this.right ? this.right.height : 0
+    const leftHeight = this.left ? this.left.height : 0
     // if not out of balance, do nothing
     // if it is ouf of balance, do I need to single or double rotate
+    if (leftHeight > rightHeight + 1) {
+      const leftRightHeight = this.left?.right ? this.left.right.height : 0
+      const leftLeftHeight = this.left?.left ? this.left.left.height : 0
+      // if double, call rotate on child then on self
+      // Double rotation
+      if (leftRightHeight > leftLeftHeight) {
+        this.left.rotateRR()
+      }
+      // Always rotate on the Node itself
+      this.rotateLL()
+    } else if (rightHeight > leftHeight + 1) {
+      const rightRightHeight = this.right?.right ? this.right.right.height : 0
+      const rightLeftHeight = this.right?.left ? this.right.left.height : 0
+      // if double, call rotate on child then on self
+      // Double rotation
+      if (rightLeftHeight > rightRightHeight) {
+        this.right.rotateLL()
+      }
+      // Always rotate on the Node itself
+      this.rotateRR()
+    }
     // if single, just call rotate on self
-    // if double, call rotate on child then on self
   }
 
   rotateRR() {
     // rotate
-    this.right.updateInNewLocation()
+    const valueBefore = this.value
+    const leftBefore = this.left
+    this.value = this.right.value
+    this.left = this.right
+    this.right = this.right.right
+    this.left.right = this.left.left
+    this.left.left = leftBefore
+    this.left.value = valueBefore
+    this.left.updateInNewLocation()
     this.updateInNewLocation()
   }
 
   rotateLL() {
     // rotate
-    this.left.updateInNewLocation()
+    const valueBefore = this.value
+    const rightBefore = this.right
+    this.value = this.left.value
+    this.right = this.left
+    this.left = this.left.left
+    this.right.left = this.right.right
+    this.right.right = rightBefore
+    this.right.value = valueBefore
+    this.right.updateInNewLocation()
     this.updateInNewLocation()
   }
 
   updateInNewLocation() {
     // Calculate the new height
+    if (!this.right && !this.left) {
+      this.height = 1
+    } else if (
+      !this.right ||
+      (this.left && this.right.height < this.left.height)
+    ) {
+      this.height = this.left.height + 1
+    } else {
+      this.height = this.right.height + 1
+    }
   }
 }
 
 // unit tests
 // do not modify the below code
-describe.skip('AVL Tree', function () {
+describe('AVL Tree', function () {
   test('creates a correct tree', () => {
     const nums = [3, 7, 4, 6, 5, 1, 10, 2, 9, 8]
     const tree = new Tree()
--- src/Tree.js
+++ src/Tree.js
@@ -4,52 +4,136 @@ import { TreeViz } from './tree-visualizer'
 import _ from 'lodash'
 
 class Tree {
+  // code goes here
   constructor() {
     this.root = null
   }
 
   add(value) {
-    // First check it it's the first node
-    if (this.root === null) {
+    if (!this.root) {
       this.root = new Node(value)
     } else {
-      let current = this.root
-      while (true) {
-        if (current.value > value) {
-          // Go left
-          if (current.left) {
-            current = current.left
-          } else {
-            current.left = new Node(value)
-            break
-          }
-        } else {
-          // Go right
-          if (current.right) {
-            current = current.right
-          } else {
-            current.right = new Node(value)
-            break
-          }
-        }
-      }
+      this.root.add(value)
     }
   }
+
   toObject() {
     return this.root
   }
 }
 
+// the Add logic will go on the Node class
 class Node {
-  constructor(value) {
+  // code also goes here
+  constructor(value = null) {
     this.value = value
     this.left = null
     this.right = null
+    this.height = 1
+  }
+
+  add(value) {
+    if (value < this.value) {
+      // go left
+      if (this.left) {
+        this.left.add(value)
+      } else {
+        this.left = new Node(value)
+      }
+      // if the left is taller than the right
+      if (!this.right || this.right.height < this.left.height) {
+        this.height = this.left.height + 1
+      }
+    } else {
+      // go right
+      if (this.right) {
+        this.right.add(value)
+      } else {
+        this.right = new Node(value)
+      }
+      // if the right is taller than the left
+      if (!this.left || this.left.height < this.right.height) {
+        this.height = this.right.height + 1
+      }
+    }
+    this.balance()
+  }
+
+  balance() {
+    // ask if is this node out of balance
+    const rightHeight = this.right ? this.right.height : 0
+    const leftHeight = this.left ? this.left.height : 0
+    // if not out of balance, do nothing
+    // if it is ouf of balance, do I need to single or double rotate
+    if (leftHeight > rightHeight + 1) {
+      const leftRightHeight = this.left?.right ? this.left.right.height : 0
+      const leftLeftHeight = this.left?.left ? this.left.left.height : 0
+      // if double, call rotate on child then on self
+      // Double rotation
+      if (leftRightHeight > leftLeftHeight) {
+        this.left.rotateRR()
+      }
+      // Always rotate on the Node itself
+      this.rotateLL()
+    } else if (rightHeight > leftHeight + 1) {
+      const rightRightHeight = this.right?.right ? this.right.right.height : 0
+      const rightLeftHeight = this.right?.left ? this.right.left.height : 0
+      // if double, call rotate on child then on self
+      // Double rotation
+      if (rightLeftHeight > rightRightHeight) {
+        this.right.rotateLL()
+      }
+      // Always rotate on the Node itself
+      this.rotateRR()
+    }
+    // if single, just call rotate on self
+  }
+
+  rotateRR() {
+    // rotate
+    const valueBefore = this.value
+    const leftBefore = this.left
+    this.value = this.right.value
+    this.left = this.right
+    this.right = this.right.right
+    this.left.right = this.left.left
+    this.left.left = leftBefore
+    this.left.value = valueBefore
+    this.left.updateInNewLocation()
+    this.updateInNewLocation()
+  }
+
+  rotateLL() {
+    // rotate
+    const valueBefore = this.value
+    const rightBefore = this.right
+    this.value = this.left.value
+    this.right = this.left
+    this.left = this.left.left
+    this.right.left = this.right.right
+    this.right.right = rightBefore
+    this.right.value = valueBefore
+    this.right.updateInNewLocation()
+    this.updateInNewLocation()
+  }
+
+  updateInNewLocation() {
+    // Calculate the new height
+    if (!this.right && !this.left) {
+      this.height = 1
+    } else if (
+      !this.right ||
+      (this.left && this.right.height < this.left.height)
+    ) {
+      this.height = this.left.height + 1
+    } else {
+      this.height = this.right.height + 1
+    }
   }
 }
 
 export default function TreeComponent() {
-  const nums = _.shuffle(_.range(20))
+  const nums = _.shuffle(_.range(40))
   const tree = new Tree()
   nums.map((num) => tree.add(num))
   const objs = tree.toObject()
