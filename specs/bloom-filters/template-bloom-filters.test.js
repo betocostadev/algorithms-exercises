19a831ed075aab74dfc939078518a5effef313d9
betocostadev <robertomarx@gmail.com>
2024-04-30 11:08:53 -0300

    Added Bloom Filters

--- specs/bloom-filters/template-bloom-filters.test.js
+++ specs/bloom-filters/template-bloom-filters.test.js
@@ -1,3 +1,12 @@
+// Bloom Filters Template
+// Do not write your code here, copy this file.
+
+// When dealing with graphs we can know for sure if a node is in a graph or not.
+// But this has the downside of needing to store the entire graph in memory.
+// Bloom filters are a way of having a probabilistic data structure. This means
+// that we can know if a node is definitely not in a graph or that it maybe is.
+// This is useful in cases where we can have false positives.
+
 // here are your hashing functions. it's not essential you know how they work
 // a library called xxhashjs is being loaded (as XXH) and we're using three different
 // instances of that as your hashing functions
@@ -14,44 +23,34 @@ const h3 = (string) =>
 // `contains` takes a string and tells you if a string is maybe in the bloom filter
 class BloomFilter {
   // you'll probably need some instance variables
-  constructor() {
-    this._array = new Array(100).fill(0)
-  }
-
   add(string) {
-    this._array[h1(string)] = 1
-    this._array[h2(string)] = 1
-    this._array[h3(string)] = 1
+    // code here
   }
   contains(string) {
-    return !!(
-      this._array[h1(string)] &&
-      this._array[h2(string)] &&
-      this._array[h3(string)]
-    )
+    // code here
   }
 }
 
 // unit tests
 // do not modify the below code
-describe('BloomFilter', function () {
+describe.skip('Template BloomFilter', function () {
   let bf
   beforeEach(() => {
     bf = new BloomFilter()
   })
-  test('returns false when empty', () => {
+  test.skip('returns false when empty', () => {
     expect(bf.contains('Brian')).toBe(false)
     expect(bf.contains('Sarah')).toBe(false)
     expect(bf.contains('Simona')).toBe(false)
   })
-  test('handles one item', () => {
+  test.skip('handles one item', () => {
     expect(bf.contains('Brian')).toBe(false)
     bf.add('Brian')
     expect(bf.contains('Brian')).toBe(true)
     expect(bf.contains('Sarah')).toBe(false)
     expect(bf.contains('Simona')).toBe(false)
   })
-  test('handles many items', () => {
+  test.skip('handles many items', () => {
     const names = [
       'Brian',
       'Simona',
